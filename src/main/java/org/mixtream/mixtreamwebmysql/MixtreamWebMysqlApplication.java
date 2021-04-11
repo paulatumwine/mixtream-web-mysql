7c43c6f68073ec248964fa74cfae5c547c287b83
Paul Tumwine <paulatumwine@gmail.com>
2021-04-11 14:00:25 -0500

    initial commit

---
+++ src/main/java/org/mixtream/mixtreamwebmysql/MixtreamWebMysqlApplication.java
@@ -0,0 +1,13 @@
+package org.mixtream.mixtreamwebmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MixtreamWebMysqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MixtreamWebMysqlApplication.class, args);
+    }
+
+}
